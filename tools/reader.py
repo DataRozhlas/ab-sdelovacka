b5d7b0bc7a9366a65e907beb6cb2dd5825bf585c
datastory.cz <honza@datastory.cz>
2019-09-26 13:15:05 +0200

    beta

--- tools/reader.py
+++ tools/reader.py
@@ -13,7 +13,7 @@ oid = 0
 txts = {}
 cmnts = {}
 
-for odst in soup.find_all('p', class_='c0'):
+for odst in soup.find_all('p', class_='c12'):
     oid += 1
     tx = markdown.markdown(odst.text).replace('<p>', '').replace('</p>', '')
     for wr in [w for w in tx.split() if '[' in w]:
@@ -23,7 +23,7 @@ for odst in soup.find_all('p', class_='c0'):
 
     for comment in odst.find_all('sup'):
         cid = comment.find('a').get('href').replace('#', '')
-        for cpar in soup.find('a', id=cid).parent.parent.find_all('span', class_='c4'):
+        for cpar in soup.find('a', id=cid).parent.parent.find_all('span', class_='c11'):
             if oid not in cmnts:
                 cmnts[oid] = ''
             cmnts[oid] += markdown.markdown(cpar.text).replace('<p>', '').replace('</p>', '') + '<br>'
@@ -35,6 +35,7 @@ with open('./js/data.js', 'w', encoding='utf-8') as f:
         'export const comments = ' + json.dumps(cmnts, ensure_ascii=False)
     )
 
-
 #%%
 print(len(cmnts), len(txts))
+
+#%%
