7368d15f0abc2c4856114c9e0f9473d49aab7414
datastory.cz <honza@datastory.cz>
2019-09-26 15:10:16 +0200

    fixy

--- tools/reader.py
+++ tools/reader.py
@@ -24,19 +24,21 @@ for odst in soup.find_all('p', class_=['c12', 'c6']):
     for comment in odst.find_all('sup'):
         cid = comment.find('a').get('href').replace('#', '')
         for cpar in soup.find('a', id=cid).parent.parent.find_all('span', class_='c11'):
+            ctx = cpar.text
+            ctx = ctx.replace('] (', '](').replace(']\n(', '](')
             if oid not in cmnts:
                 cmnts[oid] = ''
-            cmnts[oid] += markdown.markdown(cpar.text).replace('<p>', '').replace('</p>', '') + '<br>'
+            cmnts[oid] += markdown.markdown(cpar.text).replace('<p>', '').replace('</p>', '') + '<br><br>'
 
 #%%
 with open('./js/text.js', 'w', encoding='utf-8') as f:
     f.write(
-        'export const text = ' + json.dumps(txts, ensure_ascii=False)
+        'export const text = ' + json.dumps(txts, ensure_ascii=False, indent=4, separators=(',', ': '))
     )
 
 with open('./js/comments.js', 'w', encoding='utf-8') as f:
     f.write(
-        'export const comments = ' + json.dumps(cmnts, ensure_ascii=False)
+        'export const comments = ' + json.dumps(cmnts, ensure_ascii=False, indent=4, separators=(',', ': '))
     )
 
 #%%
