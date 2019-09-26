b7bbed0b527b15f8752ba05681dc118356e9a810
datastory.cz <honza@datastory.cz>
2019-09-26 20:56:07 +0200

    non-blocking vykreslovani, nova verze textu

--- article.md
+++ article.md
@@ -1,15 +1,29 @@
 title: "Sdělovačka"
-perex: "Lorem lorem"
-coverimg: https://interaktivni.rozhlas.cz/brexit/media/cover.jpg
-coverimg_note: "Foto <a href='https://ctk.cz'>ČTK</a>"
+perex: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia."
+coverimg: https://www.irozhlas.cz/sites/default/files/styles/zpravy_snowfall/public/uploader/untitled_design_190904-123715_ako.jpg?itok=kd5XpAyK
+coverimg_note: "Foto koláž iROZHLAS.cz"
 styles: []
 libraries: [] #jquery, d3, highcharts, datatables
-options: [noheader, nopic] #wide, noheader (, nopic)
+options: [] #wide, noheader (, nopic)
 ---
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+
+## Obsah
+
+- [Obvinění a jejich role](#par_1) 
+- [4 trestní oznámení](#par_184)
+- [Zrod Čapího hnízda](#par_194)
+- [2 negativní posudky](par_283)
+- [Hledání akcionářů](#par_310)
+- [Důkazy z médií](#par_329)
+- [Důkazy z bank](#par_342)
+- [Znalecký posudek](#par_358)
+- [Výslechy svědků](#par_369)
+- [Výslech Andreje Babiše](#par_465)
+- [Trestní kvalifikace](#par_474)
+- [Duševní choroby](#par_577)
+- [Malý a střední podnik?](#par_593)
+- [Zastření skutečného vlastníka](#par_645)
 
 <wide>
 	<div id="textbx"></div>
 </wide>
-
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.
--- js/script.js
+++ js/script.js
@@ -1,19 +1,59 @@
 ﻿import "./byeie"; // loučíme se s IE
-import { text } from "./text";
-import { comments } from "./comments";
+
+let host = "https://data.irozhlas.cz"
+if (location.hostname === "localhost") {
+  host = 'http://localhost'
+}
+
+function drawIt(text, comments) {
+  let pageLen = 0
+    let pageNum = 0
+    Object.keys(text).forEach(o => {
+      pageLen += text[o].length
+      let tmp = `<div id="par_${o}" class="row"><div class="txt">${text[o]}</div>`
+      if (o in comments) {
+        tmp += `<div class="cmnt" id="cmnt_${o}">${comments[o]}</div>`
+      } else {
+        tmp += `<div class="cmnt_empty"></div>`
+      }
+      //cisla stran
+      let rw = ''
+      if (pageLen >= 3600) {
+        pageLen = 0
+        pageNum += 1
+        rw = `<div class="pagenum">-${pageNum}-</div>`
+      }
+      document.getElementById("textbx").innerHTML += tmp + `</div>` + rw
+    })
+
+    let cst= document.getElementsByClassName('cmnt')
+    Array.from(cst, e => e.addEventListener('click', ev => {
+      document.getElementById(ev.target.id).classList.add('exp')
+    }))
+}
+
+let text = null
+let comments = null
+
+setTimeout(function() {
+  fetch(host + '/ab-sdelovacka/data/text.json')
+  .then(response => response.json())
+  .then(d => {
+    text = d
+    if (comments !== null) {
+      drawIt(text, comments)
+    }
+  })
+
+fetch(host + '/ab-sdelovacka/data/comments.json')
+  .then(response => response.json())
+  .then(d => {
+    comments = d
+    if (text !== null) {
+      drawIt(text, comments)
+    }
+  })
+}, 0)
 
 
-Object.keys(text).forEach(o => {
-  let tmp = `<div id="par_${o}" class="row"><div class="txt">${text[o]}</div>`
-  if (o in comments) {
-    tmp += `<div class="cmnt" id="cmnt_${o}">${comments[o]}</div>`
-  } else {
-    tmp += `<div class="cmnt_empty"></div>`
-  }
-  document.getElementById("textbx").innerHTML += tmp + `</div>`
-})
 
-let cst= document.getElementsByClassName('cmnt')
-Array.from(cst, e => e.addEventListener('click', ev => {
-  document.getElementById(ev.target.id).classList.add('exp')
-}))
